3a8a8fb4cac81aedcac69424d964dec6a713e285
abin c joseph <abincjoseph77@gmail.com>
2024-11-07 14:51:09 +0530

    arry looping

--- src/list/List.js
+++ src/list/List.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ListItem from "./ListItem";
 
-const obj =[
+const arr =[
     {
         title:'october',
         descr:'bla bla bal bla bla bal',
         isActive:true
     },
     {
-        title:'novmber',
+        title:'november',
         descr:'bla bla bal bla bla bal',
         isActive:false
     },
@@ -24,8 +24,12 @@ class List extends React.Component{
     render(){
         return(
             <div className="app-list">
-            <ListItem title={obj.title}  descr={obj.descr} isActive={obj.isActive}/>
-      </div>
+            {
+            arr.map((obj)=>{ 
+                return <ListItem key={obj.title} title={obj.title}  descr={obj.descr} isActive={obj.isActive}/> //this is array itration or looping  which is used in map() method 
+            })                                                                                  //map return the list items
+            }
+           </div>
         );
     } 
 }
